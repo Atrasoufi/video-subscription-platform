d88f1b566906d842ba6ff44a7e46949d03e1f394
Zeinab Soofi <z.soofi.n@gmail.com>
2026-09-19 14:12:00 +0330

    feat: update subscription and payment models with default values and validation method

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Plan, Subscription, Payment
 
-# Register your models here.
+
+
+@admin.register
+class PlanAdmin(admin.ModelAdmin):
+     list_display = ('title', 'price', 'duration_days', 'is_active')
+
+@admin.register
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'start_date', 'end_date', 'is_active')
+    list_filter = ('is_active', 'end_date')
+    
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'amount', 'status', 'created_at')
+    list_filter = ('status',)
+    
--- subscriptions/models.py
+++ subscriptions/models.py
@@ -9,7 +9,7 @@ class Plan(models.Model):
     title = models.CharField(max_length=225)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration_days = models.PositiveIntegerField()
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=True)
     
     def __str__(self):
         return f"{self.title} - {self.price}"
@@ -20,8 +20,8 @@ class Subscription(models.Model):
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name="subscriptions")
     
     
-    start_date = models.DateField(auto_now_add=True)
-    end_date = models.DateField()
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
 
@@ -29,6 +29,9 @@ class Subscription(models.Model):
         if not self.end_date:
             self.end_date = self.start_date + timedelta(days=self.plan.duration_days)
         super().save(*args, **kwargs)
+        
+    def is_valid(self):
+        return self.is_active and self.end_date >= timezone.now().date()
 
 
     
@@ -51,7 +54,7 @@ class Payment(models.Model):
     plan = models.ForeignKey(Plan, on_delete=models.PROTECT)
     # subscription = models.ForeignKey(Subscription)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, choices=StatusChoices.choices, default='PENDING')
+    status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.PENDING,)
     tracking_code = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
