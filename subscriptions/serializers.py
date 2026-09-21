1c2df70ec943952d1d4be947fd9d8a29e9a15f8d
Zeinab Soofi <z.soofi.n@gmail.com>
2026-09-21 14:25:26 +0330

    feat: add SubscriptionSerializer and PaymentSerializer for handling subscription and payment data

--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -1,6 +1,6 @@
 # subscriptions/serializers.py
 from rest_framework import serializers
-from .models import Plan
+from .models import Plan, Subscription, Payment
 
 
 class PlanSerializer(serializers.ModelSerializer):
@@ -13,6 +13,38 @@ class PlanSerializer(serializers.ModelSerializer):
             'price',
             'duration_days',
             'is_active',
+            'created_at',
+        ]
+        read_only_fields = ['id', 'created_at']
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    plan_details = PlanSerializer(source='plan', read_only=True)
+
+    class Meta:
+        model = Subscription
+        fields = [
+            'id',
+            'user',
+            'plan',
+            'price',
+            'start_date',
+            'end_date',
+            'is_active',
+        ]
+        read_only_fields = ['id', 'start_date', 'end_date', 'is_active']
+        
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = [
+            'id',
+            'user',
+            'plan',
+            'subscription',
+            'amount',
+            'status',
+            'tracking_code',
             'created_at',
         ]
         read_only_fields = ['id', 'created_at']
